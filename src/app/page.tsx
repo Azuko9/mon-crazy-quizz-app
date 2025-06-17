375bd8a8ff956de1a7313891f3b2deadac9b1bf6
Azuko9 <53079264+Azuko9@users.noreply.github.com>
2025-06-17 21:21:36 +0200

    Add navigation button to access Admin page in HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 export default function HomePage() {
+  const router = useRouter();
   return (
     <main className="min-h-screen flex flex-col justify-center items-center bg-[#281248]">
       <h1 className="text-3xl md:text-4xl font-extrabold mb-10 text-white text-center drop-shadow">
@@ -12,6 +13,12 @@ export default function HomePage() {
         className="mx-auto w-64 max-w-full rounded-lg "
 
       />
+      <button
+        className="bg-blue-700 text-white px-6 py-2 rounded mt-4 shadow hover:bg-blue-800"
+        onClick={() => router.push("/admin/quizz")}
+      >
+        Accès Admin
+      </button>
     </main>
   );
 }
